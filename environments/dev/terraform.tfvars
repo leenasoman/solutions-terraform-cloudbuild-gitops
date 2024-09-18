b71a7374ff47db8f01b9b14eec17b2089493db76
Leena <lsoman@google.com>
2024-09-18 16:49:39 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="lsoman-test-1"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "lsoman-test-1-tfstate"
     prefix = "env/prod"
   }
 }
